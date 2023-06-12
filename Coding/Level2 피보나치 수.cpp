2768592c1d08fc05f48a26e258fc6c9cb328b1e8
Yeoncheol-Kim <koo05131@yahoo.co.kr>
2023-06-13 00:20:30 +0900

    Create Level2 피보나치 수.cpp

---
+++ Coding/Level2 피보나치 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int n1 = 0, n2 = 1;
+    for(int i = 2; i <= n; i++)
+    {
+        answer = (n1 + n2) % 1234567;
+        n1 = n2;
+        n2 = answer;
+    }
+    return answer % 1234567;
+}
